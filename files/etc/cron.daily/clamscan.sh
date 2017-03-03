4780bae2a85dbcc4652c4edceb2c6a73a7aa5fa3
amunter <munter@gmail.com>
2017-03-03 11:48:11 -0500

    more clamav fixes
    got the scan to run well and store log output in a new directory.

--- files/etc/cron.daily/clamscan.sh
+++ files/etc/cron.daily/clamscan.sh
@@ -3,8 +3,8 @@
 # 17/01/2014 (dd/mm/yy)
 # copyleft free software
 
-LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log"; 
-EMAIL_MSG="Please see the log file attached."; 
+LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
+EMAIL_MSG="Please see the log file attached.";
 EMAIL_FROM="ncnrhelp@nist.gov";
 EMAIL_TO="ncnrhelp@nist.gov";
 DIRTOSCAN="/home";
@@ -19,7 +19,7 @@ if [ "$TODAY" == "6" ];then
  echo "Starting a full weekend scan.";
 
  # be nice to others while scanning the entire root
- nice -n5 clamscan -ri / --exclude-dir=/dev/ &>"$LOGFILE";
+ nice -n5 clamscan -ri / --exclude-dir=/dev/ --exclude-dir=/sys/ --exclude-dir=/proc/ &>"$LOGFILE";
 else
  DIRSIZE=$(du -sh "$DIRTOSCAN" 2>/dev/null | cut -f1);
 
@@ -29,12 +29,12 @@ else
  clamscan -ri "$DIRTOSCAN" &>"$LOGFILE";
 fi
 
-# get the value of "Infected lines" 
-MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3); 
+# get the value of "Infected lines"
+MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3);
 
-# if the value is not equal to zero, send an email with the log file attached 
-if [ "$MALWARE" -ne "0" ];then 
-  #using heirloom-mailx below 
-  echo "$EMAIL_MSG"|mail -a "$LOGFILE" -s "Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO"; 
-fi 
+# if the value is not equal to zero, send an email with the log file attached
+if [ "$MALWARE" -ne "0" ];then
+  #using heirloom-mailx below
+  echo "$EMAIL_MSG"|mail -a "$LOGFILE" -s "Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO";
+fi
 exit 0
